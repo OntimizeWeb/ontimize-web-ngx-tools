cd0e2c9f07fc9d9dfad7a9e152e4c5ca2e333d9d
gonzalo.martinez <gonzalo.martinez@imatia.com>
2017-10-13 15:29:06 +0200

    Adding libraries assets

---
+++ bin/assets/assets.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+'use strict';
+const shell = require("shelljs");
+
+function addLibrariesAssets() {
+  addOMapModuleAssets();
+  addFlagIconsAssets();
+}
+
+
+function addOMapModuleAssets() {
+  if (shell.test('-d', './node_modules/ontimize-web-ngx-map')) {
+    shell.cp('-R',
+      './node_modules/ontimize-web-ngx-map/leaflet/**',
+      './dist/assets/css/leaflet');
+
+    shell.cp('-R',
+      './node_modules/ontimize-web-ngx-map/leaflet/images/marker*',
+      './dist/assets');
+  }
+}
+
+
+function addFlagIconsAssets() {
+  if (shell.test('-d', './node_modules/flag-icon-css') && !shell.test('-d', './dist/assets/flags')) {
+    shell.cp('-R',
+      './node_modules/flag-icon-css/flags/**',
+      './dist/assets/assets/flags');
+  }
+}
+
+
+module.exports = addLibrariesAssets;
